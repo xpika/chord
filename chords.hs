aad96c18794415ed038e86ef5884e61d5badc220
alan hawkins <hawk.alan@gmail.com>
2013-10-18 01:04:52 +1100

    added code

---
+++ chords.hs
@@ -0,0 +1,28 @@
+import Music.Diatonic 
+import Music.Diatonic.Chord
+import Data.List
+import Data.Maybe
+
+standardTuningFirstFret = [E,A,D,G,B,E]
+
+applyNTimes f 0 x = x
+applyNTimes f n x = applyNTimes f (n-1) (f x)
+
+standardTuningFrets = map (\n -> (map (canonize . applyNTimes sharp n) standardTuningFirstFret)) [0..] 
+
+standardTuningFirstFourFrets = take 4 standardTuningFrets
+standardTuningFirstFourFretsStrings  = Data.List.transpose standardTuningFirstFourFrets 
+
+majorFingerings chord = sequence $ map (filter (flip elem (extractChord chord) )) ( standardTuningFirstFourFretsStrings )
+
+chordPositions chord =  map (map fromJust ) $ map (map (uncurry (flip elemIndex) )) $ map (zipWith (,) standardTuningFirstFourFretsStrings   )  (majorFingerings chord)
+
+renderString :: Int -> String
+renderString  =   (\n -> map (\x->if x==n then 'x' else '-') [1,2,3,4]) 
+
+renderChords chord = concat $ map  ( unlines) $ map Data.List.transpose $ map ( map renderString) (chordPositions chord)
+
+extractChord (note,chord) = map snd $ degrees $ chord note
+
+
+
