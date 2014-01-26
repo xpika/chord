28cced8811807eb0f3482ec0eec9127eeed0d1c9
Alan Hawkins <hawk.alan@gmail.com>
2014-01-26 18:16:51 +1100

    reordered functions

--- Music/Instrument/Chord.hs
+++ Music/Instrument/Chord.hs
@@ -42,38 +42,42 @@ data ControlAnnotation = AnnotateNote | AnnotatePosition | AnnotateMarking
 
 data Instrument = Guitar | Piano
 
-rotations =  reverse . (\list -> map (\n -> (take (length list) . drop (length list -n)) (cycle list)) [1..length list] )
+renderChords :: Deg s Note => (a -> s) -> a -> [Char]
+renderChords = renderChordsFirstFiveFretsWithMaximumHeightOfFour AnnotateMarking standardTuning
+
+renderChords' annotate_notes tuning chordForm chordRoot  = map unlines $ intersperse ["       "] $ map Data.List.transpose $ 
+  map (\(v,b) -> renderFretBoardHorizontal chordRoot chordForm annotate_notes tuning v b) (zip (chordPositionsVertical) [0..])
+    where chordPositionsVertical = positionsVertical (chordRoot,chordForm) tuning
 
 levelChord = map (flip mod (12::Int))
 
 inversions = map  sequenceDegrees  . rotations 
 
+rotations =  reverse . (\list -> map (\n -> (take (length list) . drop (length list -n)) (cycle list)) [1..length list] )
+
 sequenceDegrees ds = scanl1 (\x y-> x + mod (y-x) (12::Int)) ds
 
-noteToChromaticIndex note =   fromJust (findIndex (flip equiv note) chromaticScale)
-
-degreeToChromaticIndex degree  =   fromJust (findIndex (flip equiv degree) degreeScale')
+noteToChromaticIndex note = fromJust (findIndex (flip equiv note) chromaticScale)
 
 renderPianoChord chordForm chordRoot = renderPiano (levelChord degrees)
     where degrees = extractDegrees (chordRoot,chordForm)
+    
+renderPiano positions = foldl (markPiano '*') cleanPiano positions
+
+markPiano marking piano position = replaceAt (getPianoPositionCharacterIndex position) marking piano
+
+replaceAt i v xs = map (\(x,i') -> if i==i' then v else x) $  zip xs [0..]
+
+cleanPiano = map (\x -> if elem x pianoMarkings then ' ' else x ) markedPiano
 
 renderMajorChordsWithTuning tuning = renderChordsWithTuning tuning majorChord 
-
-renderMajorChords note = renderChords majorChord note
 
 renderChordsWithTuning tuning = renderChordsFirstFiveFretsWithMaximumHeightOfFour  AnnotateMarking tuning
 
 renderChordsAnnotating :: Deg s Note => ControlAnnotation ->  (a -> s) -> a -> [Char]
 renderChordsAnnotating annotation = renderChordsFirstFiveFretsWithMaximumHeightOfFour annotation standardTuning
 
-renderChords :: Deg s Note => (a -> s) -> a -> [Char]
-renderChords = renderChordsFirstFiveFretsWithMaximumHeightOfFour AnnotateMarking standardTuning
-
 renderChordsFirstFiveFretsWithMaximumHeightOfFour a t f r = concat $ union (renderChords' a t f r) (renderChords' a (map sharp t) f r)
-
-renderChords' annotate_notes tuning chordForm chordRoot  = map unlines $ intersperse ["       "] $ map Data.List.transpose $ 
-  map (\(v,b) -> renderFretBoardHorizontal chordRoot chordForm annotate_notes tuning v b) (zip (chordPositionsVertical) [0..])
-    where chordPositionsVertical = positionsVertical (chordRoot,chordForm) tuning
 
 renderFretBoardHorizontal chordRoot chordForm annotate_notes tuning strings iteration = map (\(pos,stringIndex) -> renderString annotate_notes maximumPosition pos iteration (tuning!!stringIndex)) (zip strings [0..])
   where
@@ -93,8 +97,6 @@ renderString annotate_notes max p iteration stringTuning = map (\i->char i p) [0
         fingeringCharUnannotated 0 = 'o'
         fingeringCharUnannotated _ = '*'
         
-
-positionsVertical :: Deg s Note => (a, a -> s) -> Tuning -> [[Int]]
 positionsVertical chord tuning = map (map fromJust) $ map (map (uncurry (flip elemIndex))) $ map (zipWith (,) (firstFourFretsVertical tuning)) (notesVertical chord tuning)
 
 notesVertical chord tuning = sequence $ map (filter (flip elem (extractChord chord))) (firstFourFretsVertical tuning)
@@ -105,36 +107,26 @@ firstFourFretsVertical tuning = Data.List.transpose (firstFourFrets tuning)
 extractDegrees noteChordTuple = map (+ (noteToChromaticIndex (fst noteChordTuple))) $ map degreeToChromaticIndex  $ map fst $ degrees $ (snd noteChordTuple) (fst noteChordTuple)
 extractChord noteChordTuple = map snd $ degrees $ (snd noteChordTuple) (fst noteChordTuple)
 
+degreeToChromaticIndex degree = fromJust (findIndex (flip equiv degree) degreeScale)
+
 firstFourFrets tuning = take 4 (frets tuning)
 
-frets :: [Note] -> [[Note]]
 frets tuning = map (\n -> (map (canonize . applyNTimes sharp n) tuning)) [0..] 
+
+tuningAndPosToNote tuning pos = canonize $ applyNTimes sharp pos tuning
 
 applyNTimes f n x = iterate f x !! n
 
 standardTuning = [E,A,D,G,B,E]
 
-tuningAndPosToNote tuning pos = canonize $ applyNTimes sharp pos tuning
-
-replaceAt i v xs = map (\(x,i') -> if i==i' then v else x) $  zip xs [0..]
-   
-renderPiano positions = foldl (markPiano '*') cleanPiano positions
-  
-markPiano marking piano position   = replaceAt (getPianoPositionCharacterIndex position) marking piano
-
-cleanPiano = map (\x -> if elem x pianoMarkings then ' ' else x ) markedPiano
-            
-getPianoPositionCharacterIndex pos = i
-    where (Just i) = elemIndex (pianoMarkings !! pos) markedPiano
-
-pianoMarkings = ['a'..'l']
-
-abbreviateNote x = "cCdDefFgGaAb" !! n 
-    where (Just n) = elemIndex x chromaticScale
+abbreviateNote x = "cCdDefFgGaAb" !! fromJust (elemIndex x chromaticScale)
     
 chromaticScale = [C,sharp C,D,sharp D,E,F,sharp F,G,sharp G,A,sharp A,B]
-degreeScale = [First,Second,Third,Fourth,Fifth,Sixth,Seventh]
-degreeScale' = iterate (noteMap sharp) First
+degreeScale = iterate (noteMap sharp) First
+
+getPianoPositionCharacterIndex pos = fromJust (elemIndex (pianoMarkings !! pos) markedPiano)
+
+pianoMarkings = ['a'..'l']
 
 markedPiano = unlines
         [
