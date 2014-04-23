3b42d4a043b487507feef89be041dc00f09a58c1
alan hawkins <hawk.alan@gmail.com>
2014-04-23 18:39:21 +1000

    fixed readme

--- makeReadme.hs
+++ makeReadme.hs
@@ -12,14 +12,14 @@ main = do file <- readFile "README.md.template"
           let fileLines = lines file
           let modifiedFile = unlines $ insertAt (length fileLines -1) (
                                   (makeGhciLine (expressions!!0)) ++ "\n" ++ $(return (getExpression (expressions!!0))) 
-                                 ++(makeGhciLine (expressions!!1)) ++ "\n" ++ $(return (getExpression (expressions!!1))) 
-                                 ++(makeGhciLine (expressions!!2)) ++ "\n" ++ $(return (getExpression (expressions!!2))) 
-                                 ++(makeGhciLine (expressions!!3)) ++ "\n" ++ $(return (getExpression (expressions!!3))) 
-                                 ++(makeGhciLine (expressions!!4)) ++ "\n" ++ $(return (getExpression (expressions!!4)))
+                                ++(makeGhciLine (expressions!!1)) ++ "\n" ++ $(return (getExpression (expressions!!1))) 
+                                ++(makeGhciLine (expressions!!2)) ++ "\n" ++ $(return (getExpression (expressions!!2))) 
+                                ++(makeGhciLine (expressions!!3)) ++ "\n" ++ $(return (getExpression (expressions!!3))) 
+                                ++(makeGhciLine (expressions!!4)) ++ "\n" ++ $(return (getExpression (expressions!!4)))
                                 ++(makeGhciLine (expressions!!5)) ++ "\n" ++ $(return (getExpression (expressions!!5)))
                                 ++(makeGhciLine (expressions!!6)) ++ "\n" ++ $(return (getExpression (expressions!!6)))
                                 ++(makeGhciLine (expressions!!7)) ++ "\n" ++ $(return (getExpression (expressions!!7)))
-                                ++(makeGhciLine (expressions!!8)) ++ "\n" ++ show ($(return (getExpression (expressions!!8))))
+                                ++(makeGhciLine (expressions!!8)) ++ "\n" ++ $(return (getExpression (expressions!!8)))
                                 ++(makeGhciLine (expressions!!9)) ++ "\n" ++ show ($(return (getExpression (expressions!!9))))
                               )
                              fileLines
